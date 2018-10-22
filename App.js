694aa262fac647c1b928696df0eab53771e33032
TonyBurkun <burkun.anton@gmail.com>
2018-10-22 13:35:46 +0300

    complete the project

--- App.js
+++ App.js
@@ -1,12 +1,159 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
+import { TabNavigator, createBottomTabNavigator, createStackNavigator, createDrawerNavigator } from 'react-navigation'
+import AddEntry from './components/AddEntry'
+import History from './components/History'
+import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Constants } from 'expo'
+import { setLocalNotification } from './utils/helpers'
+
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
+    return (
+        <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+            <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+        </View>
+    )
+}
+
+
+const RouteConfigs = {
+    History: {
+        screen: History,
+        navigationOptions: {
+            tabBarLabel: "History",
+            tabBarIcon: ({ tintColor }) => (
+                <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+            )
+        }
+    },
+    AddEntry: {
+        screen: AddEntry,
+        navigationOptions: {
+            tabBarLabel: "Add Entry",
+            tabBarIcon: ({ tintColor }) => (
+                <FontAwesome name='plus-square' size={30} color={tintColor}/>
+            )
+        }
+    },
+    Live: {
+        screen: Live,
+        navigationOptions: {
+            tabBarLabel: 'Live',
+            tabBarIcon: ({ tintColor }) => (
+                <Ionicons name='ios-speedometer' size={30} color={tintColor} />
+            )
+        }
+    }
+};
+
+const TabNavigatorConfig = {
+    navigationOptions: {
+        header: null
+    },
+    tabBarOptions: {
+        activeTintColor: Platform.OS === "ios" ? purple : white,
+        style: {
+            height: 56,
+            backgroundColor: Platform.OS === "ios" ? white : purple,
+            shadowColor: "rgba(0, 0, 0, 0.24)",
+            shadowOffset: {
+                width: 0,
+                height: 3
+            },
+            shadowRadius: 6,
+            shadowOpacity: 1
+        }
+    }
+};
+
+const Tabs =
+    Platform.OS === "ios"
+        ? createBottomTabNavigator(RouteConfigs, TabNavigatorConfig)
+        : createMaterialTopTabNavigator(RouteConfigs, TabNavigatorConfig);
+
+
+
+const MainNavigator = createStackNavigator({
+    home: {
+        screen: Tabs,
+        navigationOptions: {
+            header: null,
+        },
+    },
+    EntryDetail: {
+        screen: EntryDetail,
+        navigationOptions: ({ navigation }) => ({
+            headerTintColor: white,
+            headerStyle: {
+                backgroundColor: purple,
+            },
+        }),
+    },
+});
+
 
 export default class App extends React.Component {
+    componentDidMount() {
+        setLocalNotification();
+    }
+
   render() {
+
+
+      // const Tabs = TabNavigator({
+      //     History: {
+      //         screen: History,
+      //         navigationOptions: {
+      //             tabBarLabel: 'History',
+      //             tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+      //         },
+      //     },
+      //     AddEntry: {
+      //         screen: AddEntry,
+      //         navigationOptions: {
+      //             tabBarLabel: 'Add Entry',
+      //             tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+      //         },
+      //     },
+      // }, {
+      //     navigationOptions: {
+      //         header: null
+      //     },
+      //     tabBarOptions: {
+      //         activeTintColor: Platform.OS === 'ios' ? purple : white,
+      //         style: {
+      //             height: 56,
+      //             backgroundColor: Platform.OS === 'ios' ? white : purple,
+      //             shadowColor: 'rgba(0, 0, 0, 0.24)',
+      //             shadowOffset: {
+      //                 width: 0,
+      //                 height: 3
+      //             },
+      //             shadowRadius: 6,
+      //             shadowOpacity: 1
+      //         }
+      //     }
+      // });
+
+
+
+
+
     return (
-      <View style={styles.container}>
-        <Text>Opening up App.js to start working on your app!</Text>
-      </View>
+        <Provider store={createStore(reducer)}>
+          <View style={{flex: 1}}>
+            <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+            {/*<Tabs/>*/}
+            <MainNavigator/>
+          </View>
+        </Provider>
     );
   }
 }
